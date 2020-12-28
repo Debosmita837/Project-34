90be1d1788ab690775af8dca829f6f209075851f
Debosmita837 <70278407+Debosmita837@users.noreply.github.com>
2020-12-28 23:07:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+//Create variables here
+
+var dog, happyDog, database, foodS, foodStock;
+
+function preload()
+{
+  //load images here
+  
+  dogImg = loadImage("dogImg.png");
+  dogImg1 = loadImage("dogImg1.png");
+
+}
+
+function setup() {
+  createCanvas(500, 500);
+  
+  database = firebase.database();
+  foodStock = database.ref('food');
+  foodStock.on("value", readStock);
+  dog = createSprite(250, 300, 150, 150);
+  dog.addImage(dogImg);
+  dog.scale = 0.2
+  
+}
+
+
+function draw() {  
+  background(46, 139, 87);
+
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(dogImg1);
+  }
+
+  drawSprites();
+  fill (255, 255, 254);
+  stroke ("black");
+  text ("Food Remaining:" + foodS, 200, 200);
+  textSize (13);
+  text ("Note: Press UP_ARROW Key To Feed Drago Milk!", 110, 10, 300, 20);
+}
+
+function readStock(data){
+  foodS = data.val();
+}
+
+function writeStock(x){
+database.ref('/').update({
+  Food:x
+})
+}
